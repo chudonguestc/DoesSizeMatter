325b9bd25e09537ae9dcdb48a321789d70139b6a
chudonguestc <chudongcd@hotmail.com>
2023-12-17 23:18:55 +0100

    added colorpleth map

--- assets/plot/geoson_processing.py
+++ assets/plot/geoson_processing.py
@@ -5,8 +5,8 @@ with open('geo.json') as json_data:
     geo = js.load(json_data)
 
 for i in range(len(geo['features'])):
-    geo['features'][i]['id'] = i
-
-with open("geo_id.json", "w") as outfile:
+    geo['features'][i]['id'] = geo['features'][i]['properties']['NAME']
+    # geo['features'][i]['id'] = i
+with open("geo_plotly.json", "w") as outfile:
     js.dump(geo, outfile)
 
